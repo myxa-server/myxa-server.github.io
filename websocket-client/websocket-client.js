4e895ed3945be31aebbca62a2727879b84d42e47
myxa-server <catring207@gmail.com>
2024-10-20 22:33:54 +0300

    Update websocket-client.js

--- websocket-client/websocket-client.js
+++ websocket-client/websocket-client.js
@@ -1,42 +1,46 @@
+let socket; // Объявляем переменную socket здесь, чтобы она была доступна глобально
 
-// Создаем новый WebSocket клиент
+// Функция для подключения к WebSocket серверу
 function connect() {
     const input = document.getElementById('ServerIP');
     const IP = input.value;
-    const socket = new WebSocket('wss://echo.websocket.org');
+    socket = new WebSocket(IP); // Используем переменную socket, объявленную выше
+
+    // Обработчик открытия соединения
+    socket.addEventListener('open', function (event) {
+        console.log('Connected to the WebSocket server');
+        displayMessage('Connected to the WebSocket server');
+    });
+
+    // Обработчик получения сообщения
+    socket.addEventListener('message', function (event) {
+        console.log('Message from server ', event.data);
+        displayMessage('Server: ' + event.data);
+    });
+
+    // Обработчик закрытия соединения
+    socket.addEventListener('close', function (event) {
+        console.log('Disconnected from the WebSocket server');
+        displayMessage('Disconnected from the WebSocket server');
+    });
+
+    // Обработчик ошибок
+    socket.addEventListener('error', function (event) {
+        console.error('WebSocket error: ', event);
+        displayMessage('WebSocket error: ' + event);
+    });
 }
-
-
-// Обработчик открытия соединения
-socket.addEventListener('open', function (event) {
-    console.log('Connected to the WebSocket server');
-    displayMessage('Connected to the WebSocket server');
-});
-
-// Обработчик получения сообщения
-socket.addEventListener('message', function (event) {
-    console.log('Message from server ', event.data);
-    displayMessage('Server: ' + event.data);
-});
-
-// Обработчик закрытия соединения
-socket.addEventListener('close', function (event) {
-    console.log('Disconnected from the WebSocket server');
-    displayMessage('Disconnected from the WebSocket server');
-});
-
-// Обработчик ошибок
-socket.addEventListener('error', function (event) {
-    console.error('WebSocket error: ', event);
-    displayMessage('WebSocket error: ' + event);
-});
 
 // Функция для отправки сообщения
 function sendMessage() {
     const messageInput = document.getElementById('messageInput');
     const message = messageInput.value;
-    socket.send(message);
-    displayMessage('You: ' + message);
+    if (socket) {
+        socket.send(message);
+        displayMessage('You: ' + message);
+    } else {
+        console.error('WebSocket is not connected.');
+    }
     messageInput.value = '';
 }
 
